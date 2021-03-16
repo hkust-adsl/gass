f6cae494af8e2b31084c63ad26f555ba08e04f37
Da Yan <dyanab@connect.ust.hk>
2021-03-16 15:26:58 +0800

    Use int (rather than unsigned) for stalls

--- llvm/lib/Target/GASS/GASSStallSettingPass.cpp
+++ llvm/lib/Target/GASS/GASSStallSettingPass.cpp
@@ -39,15 +39,15 @@ bool GASSStallSetting::runOnMachineFunction(MachineFunction &MF) {
 
   for (MachineBasicBlock &MBB : MF) {
     // Record activate regs
-    std::map<Register*, unsigned> ActivateRegs;
+    std::map<Register*, int> ActivateRegs;
     for (auto iter = MBB.begin(); iter != MBB.end(); ++iter) {
       MachineInstr &MI = *iter;
       // minimum stall cycle: 2 (Maybe we can change this to 1?)
-      unsigned Stalls = 2; 
+      int Stalls = 2; 
 
       // TODO: use enums to interpret TSFlags
       bool IsFixedLat = MI.getDesc().TSFlags & 1;
-      unsigned Lat = SchedModel.computeInstrLatency(&MI, false);
+      int Lat = SchedModel.computeInstrLatency(&MI, false);
 
       // 1. Insert def regs as active regs
       if (IsFixedLat) {
@@ -73,7 +73,7 @@ bool GASSStallSetting::runOnMachineFunction(MachineFunction &MF) {
         // if the next instr reads any of the current active
         for (const auto &x : ActivateRegs) {
           Register *ARegs = x.first;
-          unsigned ALat = x.second;
+          int ALat = x.second;
 
           for(const MachineOperand &MOP : next_iter->uses()) {
             if (MOP.isReg()) 
@@ -84,7 +84,7 @@ bool GASSStallSetting::runOnMachineFunction(MachineFunction &MF) {
       } else { // This is the last instr
         // The last instr needs to wait on all activate regs
         for (const auto &x : ActivateRegs) {
-          unsigned ALat = x.second;
+          int ALat = x.second;
           Stalls = std::max(ALat, Stalls);
         }
       }
@@ -92,7 +92,7 @@ bool GASSStallSetting::runOnMachineFunction(MachineFunction &MF) {
       // 3. Update activate regs
       for (auto areg_iter = ActivateRegs.begin(); 
                 areg_iter != ActivateRegs.end(); ) {
-        unsigned ALat = areg_iter->second;
+        int ALat = areg_iter->second;
         ALat -= Stalls;
 
         if (ALat <= 0) 
@@ -105,13 +105,14 @@ bool GASSStallSetting::runOnMachineFunction(MachineFunction &MF) {
 
       // Special rule for BRA
       if (MI.isBranch())
-        Stalls = std::max(Stalls, unsigned(7));
+        Stalls = std::max(Stalls, int(7));
 
       // (Optional) Debug
       LLVM_DEBUG(MI.dump());
       LLVM_DEBUG(dbgs() << "[Stalls: " << Stalls << "]\n");
 
       // 4. Record Stall info
+      assert(Stalls >= 1);
       StallCycleMap[&MI] = Stalls;
     } // for each MI
   }
